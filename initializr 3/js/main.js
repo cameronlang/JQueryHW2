9faf42229b19961da4a845ff1f6edf2e942b6e00
cameronlang <Cammy@wireless-10-145-250-201.public.utexas.edu>
2016-11-16 13:43:32 -0600

    unhighlight

--- initializr 3/js/main.js
+++ initializr 3/js/main.js
@@ -2,3 +2,7 @@ function highlight (){
   $('h1').text('Hello');
   $('.jumbotron').addClass('highlight');
 }
+function unhighlight (){
+  $('h1').text('Hello, world');
+  $(.'jumbotron').removeClass('highlight');
+}
