9badc16398dc039814d0f6b07b216674e2a861c0
cameronlang <Cammy@wireless-10-145-250-201.public.utexas.edu>
2016-11-16 13:57:51 -0600

    listening to browser events

--- initializr 3/js/main.js
+++ initializr 3/js/main.js
@@ -1,8 +1,8 @@
-function highlight (){
+function highlight(){
   $('h1').text('Hello');
   $('.jumbotron').addClass('highlight');
 }
-function unhighlight (){
+function unhighlight(){
   $('h1').text('Hello, world');
   $(.'jumbotron').removeClass('highlight');
 }
@@ -12,3 +12,11 @@ function hide(){
 function show(){
   $('.jumbotron').slideDown();
 }
+$(document).ready(function() {
+  $('.jumbotron').on('mouseenter', unhihglight);
+  $('.jumbotron').on('mouseleave', unhighlight);
+});
+$(document).ready(function() {
+  $('.jumbotron').on('mouseenter', show);
+  $('.jumbotron').on('mouseleave', hide);
+});
